9990e0500c3b63a4c7d440f2f5996a3c5f9c4d04
Sebastian <sebastian.smp21@gmail.com>
2020-11-12 00:05:54 -0300

    aaeeraaaaa

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -14,7 +14,7 @@ export const HeroCard = ({
 
             <div className="row no-gutters">
                 <div className="col-md-4">
-                    <img src={ `../public/assets/heroes/${ id }.jpg`} className="card-img" alt={superhero} />
+                    <img src={ `./prueba/${ id }.jpg`} className="card-img" alt={superhero} />
 
                 </div>
                 <div className="col-md-8">
